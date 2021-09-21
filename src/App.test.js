efaf6a5fdbd47b9683ec436dd1b39b1e382ce6c8
Rafael Wassoaski <meireles200@hotmail.com>
2021-09-20 21:03:18 -0300

    #time 12m
    Adicionado testes simples apra verificação de saques, despósitos e saques de valores que não temos

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import App from './app';
+import App, {calcularNovoSaldo} from './app';
 
 describe('Componente principal', () => {
   describe('Quando eu abro o app do banco', () => {
@@ -19,6 +19,41 @@ describe('Componente principal', () => {
       render(<App />);
   
       expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+    })
+  });
+
+  describe('Quando realizo uma ação com o salvo', ()=>{
+    it('como sacar dinheiro', ()=>{
+      const valores={
+        transacao: 'saque',
+        valor: 80
+      }
+
+      const novoValor = calcularNovoSaldo(valores, 120);
+
+      expect(novoValor).toBe(40);
+    });
+
+    it('como depositar dinheiro', ()=>{
+      const valores = {
+        transacao: 'deposito',
+        valor: 50
+      };
+
+      const novoValor = calcularNovoSaldo(valores, 60);
+
+      expect(novoValor).toBe(110);
+    });
+
+    it('como tentar sacar um valor que eu não tenho', ()=>{
+      const valores = {
+        transacao: 'deposito',
+        valor: 50
+      };
+
+      const novoValor = calcularNovoSaldo(valores, 10);
+
+      expect(novoValor).toBe(novoValor);
     })
   })
 })
